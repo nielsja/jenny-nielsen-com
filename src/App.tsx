4e7fc7e20f65a71cffb9e8cf70387cd74475c3cf
Jennifer Nielsen <nielsja13@gmail.com>
2024-08-08 16:41:29 -0500

    have the dumb version working... doesn't have any scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import angular from "./assets/angular.svg";
+import csharp from "./assets/csharp.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useState();
+  const [imageIndex, setImageIndex] = useState(0);
+  // const [frameIndex, setFrameIndex] = useState(0);
+  // const [scrollPosition, setScrollPosition] = useState(0);
+
+  const html = document.documentElement;
+  const canvas = document.getElementById("main-page") as HTMLCanvasElement;
+  const context = canvas?.getContext("2d");
+  if (canvas) {
+    canvas.getContext("2d");
+    canvas.width = 1158;
+    canvas.height = 770;
+  }
+
+  const images = [csharp, angular];
+
+  const image = new Image();
+  image.src = images[imageIndex];
+  image.onload = () => {
+    context?.drawImage(image, 0, 0);
+  };
+  function logFrameInfo() {
+    const scrollFraction =
+      html.scrollTop / (html.scrollHeight - window.innerHeight);
+    console.log(`frame positions:\n
+      html.scrollTop = ${html.scrollTop}\n
+      html.scrollHeight = ${html.scrollHeight}\n
+      window.innerHeight = ${window.innerHeight}\n
+      scrollFraction = ${scrollFraction}\n
+      Math.ceil(scrollFraction * images.length) = ${Math.ceil(
+        scrollFraction * images.length
+      )}\n
+      images.length -1 = ${images.length - 1}\n
+      `);
+  }
+  function updateImage(newFrameIndex: number) {
+    setImageIndex(newFrameIndex);
+    image.src = images[newFrameIndex];
+    context?.drawImage(image, 0, 0);
+  }
+
+  window.addEventListener("scroll", () => {
+    // logFrameInfo();
+    const scrollTop = html.scrollTop;
+    const maxScrollTop = html.scrollHeight - window.innerHeight;
+    const scrollFraction = scrollTop / maxScrollTop;
+    const imageNumber = Math.ceil(scrollFraction * images.length - 1);
+    const frameIndex = Math.min(images.length - 1, imageNumber);
+    updateImage(frameIndex);
+    requestAnimationFrame(() => updateImage(frameIndex));
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <canvas id="main-page"></canvas>
+      <p>
+        hello, world!{" "}
+        {canvas !== null ? "canvas is not null" : "canvas is null"}
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
